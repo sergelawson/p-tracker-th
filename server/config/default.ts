e93b09709a17334175fb88bc7f5b0cb718f7eb02
Serge Lawson <iamsergelawson@gmail.com>
2024-06-07 21:32:15 +0000

    git ignore config

--- server/config/default.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  port: 2002,
-  dbURI: "",
-  saltWorkFactor: 10,
-  accessTokenTtl: "30m",
-  refreshTokenTtl: "1y",
-  accessTokenKey: `accesstokenprivatekey`,
-  refreshTokenKey: `refreshtokenprivatekey`,
-};
