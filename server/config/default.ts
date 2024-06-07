318a3e94e064e630e5e2c14dc50b118a8c47331f
Serge Lawson <iamsergelawson@gmail.com>
2024-06-07 21:28:43 +0000

    config file

---
+++ server/config/default.ts
@@ -0,0 +1,9 @@
+export default {
+  port: 2002,
+  dbURI: "",
+  saltWorkFactor: 10,
+  accessTokenTtl: "30m",
+  refreshTokenTtl: "1y",
+  accessTokenKey: `accesstokenprivatekey`,
+  refreshTokenKey: `refreshtokenprivatekey`,
+};
